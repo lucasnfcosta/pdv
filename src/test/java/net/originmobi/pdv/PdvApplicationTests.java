de57472748d763debfbe8b2dd14ae3c68aae1be3
Lucas Costa <lnatanfrancacosta@gmail.com>
2023-06-18 18:52:47 -0300

    correção import pdvapplicationtests

--- src/test/java/net/originmobi/pdv/PdvApplicationTests.java
+++ src/test/java/net/originmobi/pdv/PdvApplicationTests.java
@@ -1,16 +1,7 @@
 package net.originmobi.pdv;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
-import net.originmobi.pdv.security.TestSecurityConfig;
 
 //@RunWith(SpringRunner.class)
 @SpringBootTest

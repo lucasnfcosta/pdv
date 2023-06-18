d0d7b99ce894b1dbde0566d18398ff33b13fbaa9
Lucas Costa <lnatanfrancacosta@gmail.com>
2023-06-18 17:07:58 -0300

    add junit e VendaServiceTest

---
+++ src/test/java/net/originmobi/pdv/service/VendaServiceTest.java
@@ -0,0 +1,22 @@
+package net.originmobi.pdv.service;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+import net.originmobi.pdv.model.Venda;
+
+public class VendaServiceTest {
+    
+    @Test
+    public void deveRetornarCodigoNaoNulo_seVendaNova() {
+        VendaService vendaService = new VendaService();
+
+        assertNotNull(vendaService.abreVenda(vendaNova()));
+    }
+
+    private Venda vendaNova() {
+        return new Venda(null, null, null, null, 
+        null, null, null, null, null, null, null);
+    }
+}

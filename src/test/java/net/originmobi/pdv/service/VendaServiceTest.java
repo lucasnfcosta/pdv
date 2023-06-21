9b8635b00b058408bb7f408c7f574f8f8bee6d6a
Lucas Costa <lnatanfrancacosta@gmail.com>
2023-06-21 15:50:22 -0300

    segundo caso de teste para removeProduto

--- src/test/java/net/originmobi/pdv/service/VendaServiceTest.java
+++ src/test/java/net/originmobi/pdv/service/VendaServiceTest.java
@@ -17,7 +17,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import net.originmobi.pdv.enumerado.VendaSituacao;
 import net.originmobi.pdv.model.Pessoa;
-import net.originmobi.pdv.model.Produto;
 import net.originmobi.pdv.model.Usuario;
 import net.originmobi.pdv.model.Venda;
 import net.originmobi.pdv.model.VendaProduto;
@@ -64,6 +63,17 @@ public class VendaServiceTest {
         assertTrue(isEqual);
     }
 
+    @Test
+    public void removeProdutoDeveRetornarVendaFechada_seVendaFechada() {
+        vendaService.abreVenda(vendaFechada());
+
+        String actualReturn = vendaService.removeProduto(vendaProdutoFechada().getCodigo() ,vendaAberta().getCodigo());
+
+        boolean isEqual = actualReturn.equals("ok");
+
+        assertTrue(isEqual);
+    }
+
     private Venda vendaNova() {
         return new Venda(null, null, null, null, 
         null, null, null, null, null, null, null);
@@ -93,7 +103,23 @@ public class VendaServiceTest {
         return venda;
     }
 
+    private Venda vendaFechada() {
+        Pessoa pessoa = new Pessoa("Leoncio", "Leo", "11823667898", null, null, 
+        null, null, null);
+        Usuario usuario = new Usuario(null, Aplicacao.getInstancia().getUsuarioAtual(), null, null, pessoa, null, null);
+        
+        Venda venda = new Venda(null, null, null, null, null, 
+        VendaSituacao.FECHADA, null, null, null, pessoa, usuario);
+        venda.setCodigo(Long.valueOf(96));
+
+        return venda;
+    }
+
     private VendaProduto vendaProdutoAberta() {
         return new VendaProduto(Long.valueOf(1), Long.valueOf(95), 99.99);
+    }
+
+    private VendaProduto vendaProdutoFechada() {
+        return new VendaProduto(Long.valueOf(2), Long.valueOf(96), 99.99);
     }
 }

9b0bccb3915a778bff8086200b3064e8739a2204
Lucas Costa <lnatanfrancacosta@gmail.com>
2023-06-21 11:21:57 -0300

    configuração inicial da classe de teste de VendaService

--- src/test/java/net/originmobi/pdv/PdvApplicationTests.java
+++ src/test/java/net/originmobi/pdv/PdvApplicationTests.java
@@ -1,9 +1,11 @@
 package net.originmobi.pdv;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class PdvApplicationTests {
 
--- src/test/java/net/originmobi/pdv/service/VendaServiceTest.java
+++ src/test/java/net/originmobi/pdv/service/VendaServiceTest.java
@@ -1,14 +1,38 @@
 package net.originmobi.pdv.service;
 
 import static org.junit.Assert.assertNotNull;
-import org.junit.Test;
-import net.originmobi.pdv.model.Venda;
 
-public class VendaServiceTest {    
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import net.originmobi.pdv.model.Venda;
+import net.originmobi.pdv.singleton.Aplicacao;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class VendaServiceTest {
+
+    @Autowired
+    VendaService vendaService;
+
+    @Before
+    public void init() {
+        TestingAuthenticationToken testingAuth = new TestingAuthenticationToken("gerente", "123");
+        
+        SecurityContextHolder.getContext().setAuthentication(testingAuth);
+    }
+
     @Test
     public void deveRetornarCodigoNaoNulo_seVendaNova() {
-        VendaService vendaService = new VendaService();
+        System.out.println(Aplicacao.getInstancia().getUsuarioAtual());
 
+        System.out.println(vendaService);
         assertNotNull(vendaService.abreVenda(vendaNova()));
     }
 

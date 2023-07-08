e917691a1e8d6e61c661216b1d990e761e555e41
Lucas Costa <lnatanfrancacosta@gmail.com>
2023-07-08 15:43:08 -0300

    criação de subdiretório de testes de sistema

--- src/test/java/net/originmobi/pdv/service/VendaServiceTest.java
+++ src/test/java/net/originmobi/pdv/service/VendaServiceTest.java
@@ -1,6 +1,5 @@
 package net.originmobi.pdv.service;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
